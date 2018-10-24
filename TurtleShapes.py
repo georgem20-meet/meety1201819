693812a99f591896d6024470f79d5315eead81b1
georgem20-meet <georgem20@meet.mit.edu>
2018-10-24 12:23:47 -0400

    hi

---
+++ TurtleShapes.py
@@ -0,0 +1,22 @@
+import turtle
+# for star in range(5):
+# 	turtle.forward(200)
+# 	turtle.right(145)
+# turtle.mainloop()
+
+
+turtle.begin_fill()
+turtle.pendown()
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(45)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(45)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(20)
+turtle.end_fill()
+turtle.mainloop()
