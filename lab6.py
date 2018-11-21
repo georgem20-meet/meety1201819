2dd2c5074e110644cea51b5576c2e455289aa66d
georgem20-meet <georgem20@meet.mit.edu>
2018-11-21 12:02:45 -0500

    lab6.py

---
+++ lab6.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+class Square(Turtle):
+	def __init__(self,size,color):
+		Turtle.__init__(self)
+		self.shapesize (size)
+
+import turtle
+import random
+
+def change_color():
+    R = random.random()
+    B = random.random()
+    G = random.random()
+
+    turtle.color(R, G, B)
+
+def turn_and_change_color(x, y):
+    turtle.left(10)
+    change_color()
+
+turtle.onscreenclick(turn_and_change_color)
+def move_forward():
+    turtle.forward(1)
+    turtle.ontimer(move_forward, 25)
+
+move_forward()
+
+turtle.mainloop()
